1541326df6628fbe05657c2c3b499eeac5d356e3
Harshil Panchal <harshilpanchal00@gmail.com>
2021-04-25 23:57:33 -0700

    finished part3

--- prac.js
+++ prac.js
@@ -33,6 +33,7 @@ prices = discountPrice([100, 200, 300], 0.5);
 console.log(prices);
 */
 
+/*
 let student = {
     name: 'Sarah',
     major: 'Computer Science',
@@ -46,3 +47,45 @@ let student = {
 };
 
 console.log(student.courseLoad[0]);
+*/
+
+/*
+function modifyArray(array, callback) {
+    const newArr = [];
+    console.log('in modifyArray');
+    for(let i = 0; i < array.length; i++) {
+        newArr.push(callback(array[i]));
+    }
+    console.log(newArr);
+    return newArr;
+}
+
+function doSomething(num) {
+    console.log('in doSomething');
+    return num * 2;
+}
+
+modifyArray([1, 2, 3], doSomething);
+*/
+
+function printNums() {
+    console.log(1);
+    setTimeout(function() {console.log(2);}, 1000);
+    setTimeout(function() {console.log(3);}, 0);
+    console.log(4);
+}
+
+printNums();
+
+
+function printSum() {
+    debugger
+    let num1 = parseInt(document.getElementById("num1").value);
+    let num2 = parseInt(document.getElementById("num2").value);
+    document.getElementById("sum").innerHTML = "Sum: " + calculateSum(num1, num2);
+}
+
+function calculateSum(num1, num2) {
+    let result = num1 + num2
+    return result
+}
